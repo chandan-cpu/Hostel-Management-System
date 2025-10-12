445bdc5f5369e7536a05267d40e6806f0ba20818
Chandan Boruah <chandanbo345@gmail.com>
2025-10-12 12:50:33 +0530

    in heare write the logic of the add student details and display the student data created api

--- backend/controlers/admin.con.js
+++ backend/controlers/admin.con.js
@@ -1,6 +1,8 @@
-const Room=require('../models/rooms.model')
+const { get } = require('mongoose');
+const Room = require('../models/rooms.model')
+const Student = require('../models/student.model')
 
- const addRoom=async (req, res) => {
+const addRoom = async (req, res) => {
   try {
     // const newRoom = new Room(req.body);
     const { roomName, price, capacity, status, OccupiedUserName, OccupiedUserId, floor, amenities } = req.body;
@@ -39,7 +41,43 @@ const getRooms = async (req, res) => {
   }
 };
 
+
+const addStudentToRoom = async (req, res) => {
+  try {
+    const newStudent = new Student(req.body);
+    await newStudent.save();
+    res.status(201).json(newStudent);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getStudents = async (req, res) => {
+  try {
+    const students = await Student.find();
+    res.json(students);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+
+};
+
+const updateStudentFees = async (req, res) => {
+  try {
+    const { dues } = req.body;
+
+    const updateDues = await Student.findByIdAndUpdate(req.params.id, { dues: dues }, { new: true });
+    res.status(200).json(updateDues);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+
+}
+
+
 module.exports = {
   addRoom,
-  getRooms
+  getRooms,
+  addStudentToRoom,
+  getStudents, updateStudentFees
 };
